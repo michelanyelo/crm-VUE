ca21861a4df0e33e0a5f897d627ab91324586808
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-03 17:38:22 -0300

    "feat: integrate FormKit for enhanced form handling"

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import './assets/main.css'
-
+import { plugin, defaultConfig } from '@formkit/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.use(plugin, defaultConfig)
 app.use(router)
 
 app.mount('#app')
--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { FormKit } from '@formkit/vue';
 import RouterLink from '@/components/UI/RouterLink.vue';
 import HeadingComp from '@/components/UI/HeadingComp.vue';
 
@@ -15,5 +16,13 @@ defineProps({
             <RouterLink :to="{ name: 'inicio' }">Volver</RouterLink>
         </div>
         <HeadingComp>{{ titulo }}</HeadingComp>
+
+        <div class="mx-auto mt-10 bg-white shadow">
+            <FormKit type="form">
+                <FormKit type="text" label="Nombre" placeholder="Nombre del cliente"
+                    help="Coloca el nombre del cliente que deseas registrar" validation="required"
+                    :validation-messages="{ required: 'El nombre del cliente es obligatorio' }" />
+            </FormKit>
+        </div>
     </div>
 </template>
