1ecb1f607e2333393e3f2177c0ae3258229eada1
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-03 17:51:22 -0300

    `Added validation messages and new fields to NuevoClienteView.vue form`

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -20,9 +20,23 @@ defineProps({
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w-2/3 py-20 px-6">
                 <FormKit type="form">
-                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente"
-                        help="Coloca el nombre del cliente que deseas registrar" validation="required"
+                    <!-- Nombre -->
+                    <FormKit type="text" label="Nombre" placeholder="Nombre del cliente" validation="required"
                         :validation-messages="{ required: 'El nombre del cliente es obligatorio' }" />
+
+                    <!-- Apellido -->
+                    <FormKit type="text" label="Apellido" placeholder="Apellido del cliente" validation="required"
+                        :validation-messages="{ required: 'El apellido del cliente es obligatorio' }" />
+
+                    <!-- Email -->
+                    <FormKit type="email" label="Email" placeholder="Correo electrónico del cliente"
+                        validation="required|email"
+                        :validation-messages="{ required: 'El correo electrónico del cliente es obligatorio', email: 'El correo electrónico no es válido' }" />
+
+                    <!-- Telefono -->
+                    <FormKit type="text" label="Teléfono" placeholder="Teléfono del cliente: +569 12345678"
+                        validation="required"
+                        :validation-messages="{ required: 'El teléfono del cliente es obligatorio' }" />
                 </FormKit>
             </div>
         </div>
