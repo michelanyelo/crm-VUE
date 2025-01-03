afe206a0badc386a55fdd7385736028ba6ef4de7
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-03 16:21:08 -0300

    `Refactor layout and styling of App and ClientesView components`

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-  <div>
+  <header class="bg-slate-800">
+    <div class="mx-auto max-w-6xl py-10">
+      <h1 class="text-4xl font-extrabold text-white">
+        Administrador de Clientes
+      </h1>
+    </div>
+  </header>
+
+  <main class="max-w-6xl mx-auto mt-10">
     <RouterView />
-  </div>
+  </main>
 </template>
