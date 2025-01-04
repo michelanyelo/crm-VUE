a1210d824f73930fb14e665cbde95c2eddca0a06
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-04 17:24:55 -0300

    `Added new client to db.json and updated ClienteComp.vue to display client status`

--- src/components/ClienteComp.vue
+++ src/components/ClienteComp.vue
@@ -11,6 +11,8 @@ const props = defineProps({
 
 const nombreCliente = computed(() => `${props.cliente.nombre} ${props.cliente.apellido}`)
 
+const estadoCliente = computed(() => props.cliente.estado)
+
 </script>
 
 <template>
@@ -24,13 +26,11 @@ const nombreCliente = computed(() => `${props.cliente.nombre} ${props.cliente.ap
       <p class="text-gray-600"> {{ cliente.puesto }} </p>
     </td>
     <td class="whitespace-nowrap px-3 py-4 text-sm">
-      <span v-if="cliente.activo"
-        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-        Activo
-      </span>
-      <span v-else class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
-        Inactivo
-      </span>
+      <button class="inline-flex rounded-full px-2 text-sm font-semibold leading-5"
+        :class="[estadoCliente ? 'bg-green-100 text-green-800' : 'bg-red-100 text-red-800']">{{ estadoCliente ? 'Activo'
+          :
+          'Inactivo' }}</button>
+
     </td>
     <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 ">
       <RouterLink :to="{ name: 'listado-clientes' }" class="text-indigo-600 hover:text-indigo-900 mr-4">
