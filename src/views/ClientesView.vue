cbf8d88cb45090c5233ae59c82c4313c945ddcf0
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-03 21:04:13 -0300

    `Added axios dependency and made API call in ClientesView.vue`

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,6 +1,18 @@
 <script setup>
 import RouterLink from '@/components/UI/RouterLink.vue';
 import HeadingComp from '@/components/UI/HeadingComp.vue';
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+    axios.get('http://localhost:4000/clientes')
+        .then(({ data }) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error(error);
+        })
+})
 
 defineProps({
     titulo: {
