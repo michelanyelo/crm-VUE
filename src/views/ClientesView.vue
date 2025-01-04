0b5ef9f53676ac33292c977cd7ee36cb5caed0c6
michelanyelo.dev <miguelmonzon.inf@gmail.com>
2025-01-03 21:14:43 -0300

    `feat: updated ClientesView.vue to include computed property and table display`

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -2,7 +2,7 @@
 import RouterLink from '@/components/UI/RouterLink.vue';
 import HeadingComp from '@/components/UI/HeadingComp.vue';
 import axios from 'axios';
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, computed } from 'vue';
 
 const clientes = ref([]);
 
@@ -19,6 +19,8 @@ defineProps({
         type: String,
     }
 })
+
+const existenClientes = computed(() => clientes.value.length > 0);
 </script>
 
 <template>
@@ -27,5 +29,33 @@ defineProps({
             <RouterLink :to="{ name: 'agregar-cliente' }">Agregar Cliente</RouterLink>
         </div>
         <HeadingComp>{{ titulo }}</HeadingComp>
+
+        <div v-if="existenClientes">
+
+            <div v-if="existenClientes" class="flow-root mx-auto  mt-10 p-5 bg-white shadow">
+                <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Nombre
+                                    </th>
+                                    <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Empresa
+                                    </th>
+                                    <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Estado
+                                    </th>
+                                    <th scope="col" class="p-2 text-left text-sm font-extrabold text-gray-600">Acciones
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else class="text-center mt-10">No hay clientes</div>
     </div>
 </template>
